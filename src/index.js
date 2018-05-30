70f99425214a5b3da780ed3439f722bab9c752b7
Huỳnh Quang Thái <thai.huynh8324@gmail.com>
2018-05-30 16:14:25 +0700

    add api get fake data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
+import faker from 'faker';
 
 const app = express();
 
@@ -11,6 +12,21 @@ app.get('/', (req, res) => {
 	res.json({
 		info: 'welcome to my world! :)'
 	});
+});
+
+app.get('/api/chat/me', (req, res) => {
+	let total = req.query.total;
+	let messages = [];
+	while (total > 0) {
+		messages.push({
+			username: faker.name.findName(),
+			avatar: faker.internet.avatar(),
+			time: faker.date.recent(),
+			message: faker.lorem.sentence()
+		});
+		total--;
+	}
+	res.send(messages);
 });
 
 app.listen(process.env.PORT, () =>
