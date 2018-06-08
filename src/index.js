78dd89131b7685495ff05c976be6ef191f127321
Huỳnh Quang Thái <thai.huynh8324@gmail.com>
2018-06-08 17:06:58 +0700

    update

--- src/index.js
+++ src/index.js
@@ -21,12 +21,83 @@ app.get('/api/chat/me', (req, res) => {
 		messages.push({
 			username: faker.name.findName(),
 			avatar: faker.internet.avatar(),
-			time: faker.date.recent(),
+			time: faker.date.between(faker.date.past(), faker.date.recent()),
 			message: faker.lorem.sentence()
 		});
 		total--;
 	}
 	res.send(messages);
+});
+
+app.get('/api/stream/user', (req, res) => {
+	const streamData = {
+		videoTitle: faker.lorem.sentence(),
+		videoViews: faker.random.number(),
+		time: faker.date.between(faker.date.past(), faker.date.recent()),
+		username: faker.name.findName(),
+		avatar: faker.internet.avatar()
+	};
+	res.send(streamData);
+});
+
+app.get('/api/stream/active', (req, res) => {
+	let total = req.query.total;
+	let activeStreams = [];
+	while (total > 0) {
+		activeStreams.push({
+			videoTitle: faker.lorem.sentence(),
+			author: faker.name.findName(),
+			videoViews: faker.random.number(),
+			videoThumbnail: faker.image.image()
+		});
+		total--;
+	}
+	res.send(activeStreams);
+});
+
+app.get('/api/course/popular', (req, res) => {
+	let total = req.query.total;
+	let popularCourses = [];
+	while (total > 0) {
+		popularCourses.push({
+			videoTitle: faker.lorem.sentence(),
+			author: faker.name.findName(),
+			learner: faker.random.number(),
+			videoThumbnail: faker.image.image(),
+			coursePrice: faker.commerce.price()
+		});
+		total--;
+	}
+	res.send(popularCourses);
+});
+
+app.get('/api/course/upcoming', (req, res) => {
+	let total = req.query.total;
+	let upcomingCourses = [];
+	while (total > 0) {
+		upcomingCourses.push({
+			videoTitle: faker.lorem.sentence(),
+			author: faker.name.findName(),
+			learner: faker.random.number(),
+			videoThumbnail: faker.image.image(),
+			coursePrice: faker.commerce.price()
+		});
+		total--;
+	}
+	res.send(upcomingCourses);
+});
+
+app.get('/api/category', (req, res) => {
+	let total = req.query.total;
+	let category = [];
+	while (total > 0) {
+		category.push({
+			title: faker.lorem.sentence(),
+			thumbnail: faker.image.image()
+		});
+		total--;
+	}
+	res.send(category);
 });
 
 app.listen(process.env.PORT, () =>
